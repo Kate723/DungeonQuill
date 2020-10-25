1057b4a452ed52bcfef958bcf0214e408f0bbaaa
Jt W <1250085549@qq.com>
2020-10-25 15:27:33 +0800

    adv

--- DungeonQuill/DataManager.cpp
+++ DungeonQuill/DataManager.cpp
@@ -11,8 +11,8 @@ void DataManager::createDb()
         exit(-1);
    
     //普通法术表
-    QSqlQuery query(spelldb);
-    query.exec("create table Spell(ID  int primary key, nameCh varchar(20), nameEn varchar(20), schoolID int, level int, isRitual bool,"
+    QSqlQuery squery(spelldb);
+    squery.exec("create table Spell(ID  int primary key, nameCh varchar(20), nameEn varchar(20), schoolID int, level int, isRitual bool,"
         "CastingTimelength int, CastingTimeremarks varchar(1000), range int, isSelfOnly bool, SpellComponmentsverbalNeed bool,"
         "SpellComponmentssomaticNeed bool, SpellComponmentsmaterialNeed bool, SpellComponmentsremark varchar(1000), DurationconcentrationNeed bool,"
         "Durationtime int, Durationremarks varchar(1000), targetNeed bool, targetNum int, AreashapeID int, Areasize int, remarks varchar(1000))");
@@ -21,8 +21,8 @@ void DataManager::createDb()
     for (int i = 0; i < 21; i++)
         qs = qs + "?,";
     qs = qs + "?)";
-    query.prepare(qs);
-    QVariantList List[30];
+    squery.prepare(qs);
+    QVariantList List[40];
     List[0] <<1001 << 1000;
     List[1] << "测试二" << "测试一";
     List[2] << "test2" << "test1";
@@ -46,17 +46,17 @@ void DataManager::createDb()
     List[20] << 3 << 2;
     List[21] << "test2" << "test1";
     for (int i = 0; i < 22; i++)
-        query.addBindValue(List[i]);
-    query.execBatch();
+        squery.addBindValue(List[i]);
+    squery.execBatch();
    
     //伤害法术表
-    query.exec("create table damageSpell(ID  int primary key, attackRollNeed int, savingThrowNeed int, savingThrowTypeID int, damageType int," 
+    squery.exec("create table damageSpell(ID  int primary key, attackRollNeed int, savingThrowNeed int, savingThrowTypeID int, damageType int," 
         "DiceRollTypediceNum int, DiceRollTypediceNumDependence int,DiceRollTypediceSideNum int)");
     qs = "insert into damageSpell values (";
     for (int i = 0; i < 7; i++)
         qs = qs + "?,";
     qs = qs + "?)";
-    query.prepare(qs);
+    squery.prepare(qs);
     for (int i = 0; i < 8; i++)
         List[i].clear();
     List[0]<< 1001 << 1000;
@@ -68,13 +68,13 @@ void DataManager::createDb()
     List[6] << 3 << 2;
     List[7] << 3 << 2;
     for (int i = 0; i < 8; i++)
-        query.addBindValue(List[i]);
-    query.execBatch();
+        squery.addBindValue(List[i]);
+    squery.execBatch();
     
     //治愈法术表
-    query.exec("create table healSpell(ID  int primary key, DiceRollTypediceNum int, DiceRollTypediceNumDependence int,"
+    squery.exec("create table healSpell(ID  int primary key, DiceRollTypediceNum int, DiceRollTypediceNumDependence int,"
         "DiceRollTypediceSideNum int)");
-    query.prepare("insert into healSpell values(?,?,?,?)");
+    squery.prepare("insert into healSpell values(?,?,?,?)");
     for (int i = 0; i < 4; i++)
         List[i].clear();
     List[0] << 1001 << 1000;
@@ -82,8 +82,8 @@ void DataManager::createDb()
     List[2] << 3 << 2;
     List[3] << 3 << 2;
     for (int i = 0; i < 4; i++)
-        query.addBindValue(List[i]);
-    query.execBatch();
+        squery.addBindValue(List[i]);
+    squery.execBatch();
 
     spelldb.close();
 
@@ -94,39 +94,39 @@ void DataManager::createDb()
     List[0].clear();
     List[0] << 1001<<1000;
     query.addBindValue(List[0]);
-    query.execBatch();
+    query.execBatch();*/
 
   
    
     //装备数据库
     QSqlDatabase equdb = QSqlDatabase::addDatabase("QSQLITE","equConnection");
-    spelldb.setDatabaseName("equ.db");
+    equdb.setDatabaseName("equ.db");
     if (!equdb.open())
         exit(-1);
 
     //普通装备表
-    query.exec("create table equipment (ID int ,name varchar(20),remark varchar(1000), type int)");
-
-    query.prepare("insert into spellBard values (?,?,?,?)");
-    for (int i = 0; i < 3; i++)
+    QSqlQuery equery(equdb);
+    equery.exec("create table equipment (ID int primary key,name varchar(20),remark varchar(1000), type int)");
+    equery.prepare("insert into equipment values (?,?,?,?)");
+    for (int i = 0; i < 4; i++)
         List[i].clear();
     List[0] << 101 << 100;
     List[1] << "test2" << "test1";
     List[2] << "test2" << "test1";
     List[3] << 0 << 0;
-    for (int i = 0; i < 3; i++)
-        query.addBindValue(List[i]);
-    query.execBatch();
+    for (int i = 0; i < 4; i++)
+        equery.addBindValue(List[i]);
+    equery.execBatch();
 
     //武器表
-    query.exec("create table weapon (ID int primary key,dN int ,dND  int ,dSN int ,tneedAmmunition  bool ,tisFinesse bool , type int,range int,"
-       " tregularRange int  tmaxRange, int)");
-    qs = "insert into Spell values (";
+    equery.exec("create table weapon (ID int primary key,dN int ,dND  int ,dSN int ,tneedAmmunition  bool ,tisFinesse bool , type int,range int,"
+       " tregularRange int , tmaxRange int)");
+    qs = "insert into weapon values (";
     for (int i = 0; i < 9; i++)
-        qs = qs + "?";
-    qs = qs + ")";
-    query.prepare(qs);
-    for (int i = 0; i < 9; i++)
+        qs = qs + "?,";
+    qs = qs + "?)";
+    equery.prepare(qs);
+    for (int i = 0; i < 10; i++)
         List[i].clear();
     List[0] << 101 << 100;
     List[1] << 3 << 2;
@@ -137,22 +137,23 @@ void DataManager::createDb()
     List[6] << 3 << 2;
     List[7] << 3 << 2;
     List[8] << 3 << 2;
-    for (int i = 0; i < 9; i++)
-        query.addBindValue(List[i]);
-    query.execBatch();
+    List[9] << 3 << 2;
+    for (int i = 0; i < 10; i++)
+        equery.addBindValue(List[i]);
+    equery.execBatch();
 
     //防御表
-    query.exec("create table amour(ID int primary key,basicAC int,type int,strength  int) ");
-    query.prepare("insert into amour values (?,?,?,?)");
-    for (int i = 0; i < 3; i++)
+    equery.exec("create table amour(ID int primary key,basicAC int,type int,strength  int) ");
+    equery.prepare("insert into amour values (?,?,?,?)");
+    for (int i = 0; i < 4; i++)
         List[i].clear();
     List[0] << 101 << 100;
     List[1] << 3 << 2;
     List[2] << 3 << 2;
     List[3] << 3 << 2;
-    for (int i = 0; i < 3; i++)
-        query.addBindValue(List[i]);
-    query.execBatch();
+    for (int i = 0; i < 4; i++)
+        equery.addBindValue(List[i]);
+    equery.execBatch();
 
     equdb.close();
 
@@ -162,19 +163,75 @@ void DataManager::createDb()
     if (!advdb.open())
         exit(-1);
 
-    qs = "create table Adventurer(ttid int primary key, ttname varchar(20),tspeed  int,tmaxHitPoint  int,tcurHitPoint  int ,"
-        "ttempHitPoint int,texp  int ,tsex  bool , tage int ,feet  int ,inch  int , tweight int , tideal varchar(20), tbond varchar(20), "
-        "tflaw varchar(20),tapperance varchar(20), tbgStory varchar(20),ttttname varchar(20),ttttid  int,tsubName varchar(20),tsubID  int ,"
-        "tid int , tname varchar(20), tremarks varchar(20),tttid  int ,tttname varchar(20), ttremarks varchar(20),tlevel  int ,"
-        "tspecialAbilityName varchar(20),tcurSpecialPoint  int , tmaxSpecialPoint int,tcurHitDice int ,tmaxHitDice  int ,ticonPath varchar(20),"
-        "cp int , sp int,ep  int ,gp  int ,pp  int, ";
+    QSqlQuery aquery(advdb);
+    qs = "create table Adventurer(ID int primary key, ttname varchar(20),tspeed  int,tmaxHitPoint  int,tcurHitPoint  int ,"
+        "ttempHitPoint int,texp  int ,tsex  bool , tage int ,feet  int ,inch  int , tweight int , tideal varchar(1000), tbond varchar(1000), "
+        "tflaw varchar(1000),tapperance varchar(1000), tbgStory varchar(1000),ttttname varchar(20),ttttid  int,tsubName varchar(20),tsubID  int ,"
+        "tid int , tname varchar(20), tremarks varchar(1000),tttid  int ,tttname varchar(20), ttremarks varchar(1000),tlevel  int ,"
+        "tspecialAbilityName varchar(20),tcurSpecialPoint  int , tmaxSpecialPoint int,tcurHitDice int ,tmaxHitDice  int ,ticonPath varchar(1000),"
+        "cp int , sp int,ep  int ,gp  int ,pp  int";
     for (int i = 0; i < 3*vnum; i++)
     {
-        qs = qs + "int";
+        qs = qs + ",v"+QString::number(i,10)+" int";
     }
     qs = qs + ")";
-    query.exec(qs);*/
+    aquery.exec(qs);
+    qs = "insert into Adventurer (ID, ttname,tspeed, tmaxHitPoint, tcurHitPoint ,ttempHitPoint, texp, tsex, tage, feet, inch , tweight, tideal, tbond,"
+        "tflaw ,tapperance , tbgStory,ttttname ,ttttid ,tsubName ,tsubID,tid , tname , tremarks ,tttid  ,tttname, ttremarks ,tlevel,"
+        "tspecialAbilityName,tcurSpecialPoint , tmaxSpecialPoint,tcurHitDice,tmaxHitDice  ,ticonPath ,cp , sp ,ep   ,gp   ,pp )values (";
+    for (int i = 0; i < 38; i++)
+        qs = qs + "?,";
+    qs = qs + "?)";
+    aquery.prepare(qs);
+    for (int i = 0; i < 39; i++)
+        List[i].clear();
+    List[0] << 1001 << 1000;
+    List[1] << "test1" <<"test2";
+    List[2] << 3 << 2;
+    List[3] << 3 << 2;
+    List[4] << 3 << 2;
+    List[5] << 3 << 2;
+    List[6] << 3 << 2;
+    List[7] << 1 << 0;
+    List[8] << 3 << 2;
+    List[9] << 3 << 2;
+    List[10] << 3 << 2;
+    List[11] << 3 << 2;
+    List[12] << "test2" << "test1";
+    List[13] << "test2" << "test1";
+    List[14] << "test2" << "test1";
+    List[15] << "test2" << "test1";
+    List[16] << "test2" << "test1";
+    List[17] << "test2" << "test1";
+    List[18] << 3 << 2;
+    List[19] << "test2" << "test1";
+    List[20] << 3 << 2;
+    List[21] << 3 << 2;
+    List[22] << "test2" << "test1";
+    List[23] << "test2" << "test1";
+    List[24] << 3 << 2;
+    List[25] << "test2" << "test1";
+    List[26] << "test2" << "test1";
+    List[27] << 3 << 2;
+    List[28] << "test2" << "test1";
+    List[29] << 3 << 2;
+    List[30] << 3 << 2;
+    List[31] << 3 << 2;
+    List[32] << 3 << 2;
+    List[33] << "test2" << "test1";
+    List[34] << 3 << 2;
+    List[35] << 3 << 2;
+    List[36] << 3 << 2;
+    List[37] << 3 << 2;
+    List[38] << 3 << 2;
+    for (int i = 0; i < 39; i++)
+        aquery.addBindValue(List[i]);
+    aquery.execBatch();
+    aquery.exec("select ID from Adventurer");
+    aquery.next();
+    qDebug() << aquery.value(0);
 
+    advdb.close();
    /*query.exec("select * from spellBard");
     while (query.next())
     {
